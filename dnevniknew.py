606d6c289e4413e0209f8a70a1aabe779cf80a39
Vannitass <93047656+Vannitass@users.noreply.github.com>
2024-02-29 01:20:45 +0300

    Add files via upload

---
+++ dnevniknew.py
@@ -0,0 +1,42 @@
+def sr(s):
+    """
+    Sum of numbers
+    """
+    suma = 0
+    for i in s:
+        suma += int(i)
+    return suma
+
+def average(s):
+    """
+    Среднее арифметическое ряда оценок
+    """
+    return sr(s) / len(s)
+
+def skolkodvoek(listofmarks, n):
+    # текущюю оценку
+    # сколько двоек до {n}
+
+def downgrageMarks(оценка):
+    """
+    Вернет список из оценок ниже до 2
+    """
+    # return [список]
+
+# сколько двоек осталось до [оценок ниже текущей]
+def  f():
+    spisok = downgrageMarks(***)
+    # в цикле
+    skolkodvoek
+
+a = input()
+summ = sr(a)
+print('Сумма оценок', summ)
+midl = summ / len(a) # средний балл
+print(' Средний балл ', ':' , midl)
+
+# Ряд оценок
+
+# До которых нужно спускаться это: текущий бал - 1, -2, -3 и так до 2 включительно.
+
+# Сколько двоек до [оценки]
